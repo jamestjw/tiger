6320cf30f15fd507c2ca250f769e535e555810ea
James Tan <james_tan97@outlook.com>
2022-09-07 18:35:47 -0400

    Introduce Batteries library. Add implemention of Graph.

---
+++ lib/table.ml
@@ -0,0 +1,25 @@
+module type TABLE = sig
+  type key
+  type 'a tbl
+
+  val empty : 'a tbl
+  val enter : 'a tbl * key * 'a -> 'a tbl
+  val look : 'a tbl * key -> 'a option
+end
+
+module type KEY = sig
+  type t
+
+  val getKey : t -> int
+end
+
+module IntMapTable (Key : KEY) : TABLE = struct
+  module IntMap = Caml.Map.Make (Int)
+
+  type 'a tbl = 'a IntMap.t
+  type key = Key.t
+
+  let empty = IntMap.empty
+  let enter (t, k, v) = IntMap.add (Key.getKey k) v t
+  let look (t, k) = IntMap.find_opt (Key.getKey k) t
+end
