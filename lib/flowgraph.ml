8432cd9fbd521c326b198b3b0f62fc821a3ff713
James Tan <james_tan97@outlook.com>
2024-05-25 18:00:35 -0400

    Add function that builds liveMap

--- lib/flowgraph.ml
+++ lib/flowgraph.ml
@@ -6,9 +6,13 @@ module Flow = struct
 
   type flowgraph =
     | FGRAPH of {
+        (* Graph where each node is an instruction or basic block *)
         control : Graph.graph;
+        (* Table of defined temporaries at each node *)
         def : Temp.temp list Graph.Table.tbl;
+        (* Table of used temporaries at each node *)
         use : Temp.temp list Graph.Table.tbl;
+        (* Table that keeps track of whether each node is a `move` *)
         ismove : bool Graph.Table.tbl;
       }
 
