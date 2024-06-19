a0bf79e4e5bfb42b4b2d67568d85ddac2e6cbee8
James Tan <james_tan97@outlook.com>
2024-06-19 01:56:17 -0400

    Use `Command_unix` for argument parsing

--- bin/main.ml
+++ bin/main.ml
@@ -1,29 +1,27 @@
 open Tiger
-open Base
+open Core
 
-let input_file = ref ""
-let output_file = ref ""
-let usage_msg = "tiger -o output-file input-file"
+let command =
+  Command.basic
+    ~summary:
+      "Compiles a Tiger file to RISC-V assembly.\n\
+       Usage: tiger input-file -o output-file"
+    (let%map_open.Command input_file =
+       anon ("filename" %: Filename_unix.arg_type)
+     and output_file =
+       flag "-o" (optional string)
+         ~doc:"output-filename Output RISC-V assembly file."
+     in
+     fun () ->
+       let outc =
+         match output_file with
+         | Some output_file ->
+             Stdio.Out_channel.create output_file ~append:false
+               ~fail_if_exists:false
+         | None -> Stdio.Out_channel.stdout
+       in
+       Stdlib.Printf.fprintf outc "%s"
+         (Driver.compile_file ~filename:input_file);
+       Stdio.Out_channel.close outc)
 
-let speclist =
-  [ ("-o", Stdlib.Arg.Set_string output_file, "Set output file name") ]
-
-let add_input_file fn = input_file := fn
-
-(* TODO: Use a more reliable library to handle this *)
-let main () =
-  Stdlib.Arg.parse speclist add_input_file usage_msg;
-  if String.(!input_file = "") then (
-    Stdio.print_string "Error: Missing input file\n";
-    Stdlib.exit 1);
-
-  let outc =
-    if String.(!output_file = "") then Stdio.Out_channel.stdout
-    else
-      Stdio.Out_channel.create !output_file ~append:false ~fail_if_exists:false
-  in
-
-  Stdlib.Printf.fprintf outc "%s" (Driver.compile_file ~filename:!input_file);
-  Stdio.Out_channel.close outc
-
-let () = main ()
+let () = Command_unix.run command
