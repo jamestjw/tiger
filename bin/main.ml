6d156174cf9ba2b5d17c4e4fd81665831a6c6bbf
James Tan <james_tan97@outlook.com>
2024-06-19 02:46:04 -0400

    Allow input file to be read via stdin.

--- bin/main.ml
+++ bin/main.ml
@@ -4,15 +4,21 @@ open Core
 let command =
   Command.basic
     ~summary:
-      "Compiles a Tiger file to RISC-V assembly.\n\
+      "Compiles a Tiger file to RISC-V assembly. Input file may also be read \
+       via stdin.\n\
        Usage: tiger input-file -o output-file"
     (let%map_open.Command input_file =
-       anon ("filename" %: Filename_unix.arg_type)
+       anon (maybe ("filename" %: Filename_unix.arg_type))
      and output_file =
        flag "-o" (optional string)
          ~doc:"output-filename Output RISC-V assembly file."
      in
      fun () ->
+       let inc =
+         match input_file with
+         | Some input_file -> Stdio.In_channel.create input_file
+         | None -> Stdio.In_channel.stdin
+       in
        let outc =
          match output_file with
          | Some output_file ->
@@ -21,7 +27,7 @@ let command =
          | None -> Stdio.Out_channel.stdout
        in
        Stdlib.Printf.fprintf outc "%s"
-         (Driver.compile_file ~filename:input_file);
+         (Driver.compile_channel ?filename:input_file inc);
        Stdio.Out_channel.close outc)
 
 let () = Command_unix.run command
